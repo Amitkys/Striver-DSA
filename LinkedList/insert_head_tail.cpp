9020f1b53399f9973e1d9314ba6ff37b6cf789de
KYs <amitkys59@gmail.com>
2024-07-01 12:10:13 +0530

    used another way to insert node in linkedlist

--- LinkedList/insert_head_tail.cpp
+++ LinkedList/insert_head_tail.cpp
@@ -44,3 +44,50 @@ int main()
     insertAtTail(tail, 32);
     print(head);
 }
+
+// another way to insert
+/*
+
+#include <iostream>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int data){
+        this -> data = data;
+        this -> next = nullptr;
+    }
+};
+void insertATHead(Node* &head, int data){
+    if(head == nullptr){
+        head = new Node(data);
+    }
+
+    Node* temp = new Node(data);
+    temp -> next = head;
+    head = temp;
+
+}
+void traverse(Node* &head){
+    if(head == nullptr){
+        cout << "Linkedlist is empty" << endl;
+        return;
+    }
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+
+}
+int main(){
+  Node* head = new Node(45);
+  insertATHead(head, 34);
+  traverse(head);
+  
+}
+
+
+*/
